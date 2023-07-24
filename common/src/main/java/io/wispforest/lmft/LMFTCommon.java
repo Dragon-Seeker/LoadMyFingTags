04e074f6cd2451ee2f5c4536c2361b546fdd3463
Dragon-Seeker <timekeeperguild@gmail.com>
2023-07-24 15:17:30 -0500

    Add Multiloader Configuration support as alternative

--- common/src/main/java/io/wispforest/lmft/LMFTCommon.java
+++ common/src/main/java/io/wispforest/lmft/LMFTCommon.java
@@ -1,11 +1,22 @@
 package io.wispforest.lmft;
 
+import com.google.gson.*;
+import com.mojang.logging.LogUtils;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.slf4j.Logger;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class LMFTCommon {
+
+    public static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public static final String MODID = "lmft";
 
@@ -13,18 +24,46 @@ public class LMFTCommon {
 
     public static boolean disableIngameError = Boolean.getBoolean("lmft.disable_error_output");
 
-    public static void init() {}
+    public static void init(Path configPath) {
+        File configFile = new File(configPath + File.separator + "lmft.json");
+
+        JsonObject configObject = null;
+
+        //Based on builtinServer multi loader implementation
+        try {
+            if (configFile.createNewFile()) {
+                LOGGER.info("[LMFT]: Unable to find needed config file, will attempt to create such.");
+
+                configObject = new JsonObject();
+                configObject.addProperty("disableIngameError", false);
+
+                try (FileWriter writer = new FileWriter(configFile)) {  writer.write(GSON.toJson(configObject)); }
+            } else {
+                configObject = GSON.fromJson(Files.readString(configFile.toPath()), JsonObject.class);
+
+                if(configObject == null) throw new IOException("[LMFT]: The config file was not found!");
+
+                LOGGER.info("[LMFT]: Loaded Config File!");
+            }
+        } catch (IOException exception) {
+            LOGGER.error("[LMFT]: Unable to create the needed config file, using default values!", exception);
+        } catch (JsonSyntaxException exception) {
+            LOGGER.error("[LMFT]: Unable to read the needed config file, using default values!", exception);
+        }
+
+        if(configObject != null && configObject.has("disableIngameError")) disableIngameError = disableIngameError || configObject.get("disableIngameError").getAsBoolean();
+    }
 
     public static void sendMessage(PlayerEntity entity){
-        if(LMFTCommon.areTagsCooked && !LMFTCommon.disableIngameError){
-            entity.sendMessage(
-                    Text.literal("[Load My F***ing Tags]: It seems that some tags are a bit cooked. Look at the Logs for more details on broken functions. Click me for more info about this feature.")
-                            .styled(style -> {
-                                return style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/Dragon-Seeker/LoadMyFingTags/blob/ae915c6453f0537e12604e3b0c3d0c378b2dfd88/info.md"));
-                            })
-                            .formatted(Formatting.RED, Formatting.BOLD),
-                    false
-            );
-        }
+        if(!LMFTCommon.areTagsCooked || LMFTCommon.disableIngameError) return;
+
+        entity.sendMessage(
+                Text.literal("[Load My F***ing Tags]: It seems that some tags are a bit cooked. Look at the Logs for more details on broken functions. Click me for more info about this feature.")
+                        .styled(style -> {
+                            return style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/Dragon-Seeker/LoadMyFingTags/blob/ae915c6453f0537e12604e3b0c3d0c378b2dfd88/info.md"));
+                        })
+                        .formatted(Formatting.RED, Formatting.BOLD),
+                false
+        );
     }
 }
--- forge/src/main/java/io/wispforest/lmft/forge/LMFTForge.java
+++ forge/src/main/java/io/wispforest/lmft/forge/LMFTForge.java
@@ -4,11 +4,12 @@ import io.wispforest.lmft.LMFTCommon;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(LMFTCommon.MODID)
 public class LMFTForge {
     public LMFTForge() {
-        LMFTCommon.init();
+        LMFTCommon.init(FMLPaths.CONFIGDIR.get());
 
         MinecraftForge.EVENT_BUS
                 .addListener((PlayerEvent.PlayerLoggedInEvent e) -> LMFTCommon.sendMessage(e.getEntity()));
