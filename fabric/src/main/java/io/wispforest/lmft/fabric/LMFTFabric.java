dd94a53dc245f033bd78528c57a92b5bc61847de
Dragon-Seeker <timekeeperguild@gmail.com>
2023-07-24 15:17:30 -0500

    Add Multiloader Configuration support as alternative

--- fabric/src/main/java/io/wispforest/lmft/fabric/LMFTFabric.java
+++ fabric/src/main/java/io/wispforest/lmft/fabric/LMFTFabric.java
@@ -3,12 +3,13 @@ package io.wispforest.lmft.fabric;
 import io.wispforest.lmft.LMFTCommon;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class LMFTFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LMFTCommon.init();
+        LMFTCommon.init(FabricLoader.getInstance().getConfigDir());
 
         ServerPlayConnectionEvents.JOIN.register((h, s, c) -> LMFTCommon.sendMessage(h.getPlayer()));
     }
