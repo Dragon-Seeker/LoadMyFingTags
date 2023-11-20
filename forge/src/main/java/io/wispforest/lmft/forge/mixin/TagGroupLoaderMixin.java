aa03a6ac8b3bb993c884e8f8b3d46a3600ae1b51
Dragon-Seeker <timekeeperguild@gmail.com>
2023-11-19 21:30:01 -0600

    Fix crash due to recent bug fix with TagGroup.Loader.Patch

--- forge/src/main/java/io/wispforest/lmft/forge/mixin/TagGroupLoaderMixin.java
+++ forge/src/main/java/io/wispforest/lmft/forge/mixin/TagGroupLoaderMixin.java
@@ -27,7 +27,7 @@ public class TagGroupLoaderMixin<T> {
     @Unique private static final ThreadLocal<Identifier> currentTagId = ThreadLocal.withInitial(() -> new Identifier("", ""));
 
     @Inject(method = "resolveAll(Lnet/minecraft/registry/tag/TagEntry$ValueGetter;Ljava/util/List;)Lcom/mojang/datafixers/util/Either;", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void preventTagsFromFailingToLoad(TagEntry.ValueGetter<T> valueGetter, List<TagGroupLoader.TrackedEntry> list, CallbackInfoReturnable<Either<Collection<TagGroupLoader.TrackedEntry>, Collection<T>>> cir, HashSet builder, List<TagGroupLoader.TrackedEntry> list2){
+    private void preventTagsFromFailingToLoad(TagEntry.ValueGetter<T> valueGetter, List<TagGroupLoader.TrackedEntry> list, CallbackInfoReturnable<Either<Collection<TagGroupLoader.TrackedEntry>, Collection<T>>> cir, LinkedHashSet builder, List<TagGroupLoader.TrackedEntry> list2){
         if(list2.isEmpty()) return;
 
         LOGGER.error(
